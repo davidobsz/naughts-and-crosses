09f640061f24f03951ca678056cce92a938b8c28
Dawid Obszanski <dawidobszanski@icloud.com>
2022-01-25 16:27:50 +0000

    fixed bug allowing user to press already selected object

--- static/scripts/games.js
+++ static/scripts/games.js
@@ -6,15 +6,17 @@ function goToIndex() {
 
 let vals = null
 function value(value) {
-    console.log(value)
+    
+    // console.log("value: ",value)
     vals = Number(value)
-    console.log("vals:", vals)
-    console.log(typeof vals)
+    //console.log("vals:", vals)
+    //console.log(typeof vals)
     find(vals)
+    
 }
 
 
-console.log("vals: " + vals)
+
 
 let player1 = null;
 let player2 = null;
@@ -22,7 +24,7 @@ let oppositePlayer = null;
 let currentPlayer = null;
 
 function symbol(symbol) {
-    console.log(symbol)
+    // console.log(symbol)
     
     if (symbol === "x"){
         player1 = "x"
@@ -49,19 +51,19 @@ let sum = 0
 function clear() {
     document.getElementById("pick-symbol").innerHTML = null
     document.getElementById("p1-symbol").innerHTML = null
-    if (sum % 2 == 0) {
+    if (sum == 0) {
         playerTurn = document.getElementById("player-turn").innerHTML = player1
         document.getElementById("player-turn").innerHTML = `Player Turn: ${player1}`
-        sum++
-    } else if (sum % 2 == 1) {
+        
+    } else {
         playerTurn = document.getElementById("player-turn").innerHTML = player2
         document.getElementById("player-turn").innerHTML = `Player Turn: ${player2}`
-        sum++
+        
     }
     
-    console.log("playerTurn: " + playerTurn)
-    console.log("player1", player1)
-    console.log("player2", player2)
+    // console.log("playerTurn: " + playerTurn)
+    // console.log("player1", player1)
+    // console.log("player2", player2)
 }
 
 
@@ -73,30 +75,101 @@ function find(vals){
 
 let count = 0 
 function removes(vals){
-    if ((playerTurn == player1) && (count % 2 == 0)) {
-        console.log("player1 turn")
+    //console.log("count", vals)
+    if ((playerTurn == player1)) {
+        // console.log("player1 turn")
         document.getElementById(`${vals}`).innerText = playerTurn
         document.getElementById(`${vals}`).className = playerTurn
         document.getElementById("player-turn").innerText = `Player Turn: ${player2}`
         document.getElementById("player-turn").className = player2
         count++;
         playerTurn = player2
+        sets(vals, player1)
+        bugFix()
         checksBefore()
         checks(player1)
+    
     }
-    else if ((playerTurn == player2) && (count % 2 == 1)){
-        console.log("player 2 turn")
+    else if ((playerTurn == player2)){
+        // console.log("player 2 turn")
         document.getElementById(`${vals}`).innerText = playerTurn
         document.getElementById(`${vals}`).className = playerTurn
         document.getElementById("player-turn").innerText = `Player Turn: ${player1}`
         document.getElementById("player-turn").className = player1
         count++;
         playerTurn = player1
+        sets(vals, player2)
+        bugFix()
+        
         checksBefore()
         checks(player2)
+        
     }
 }  
 
+
+let arr = []
+let arr2 = {}
+let gameStats = []
+let o = 0
+function sets(values, playr){
+    console.log(values)
+    arr[o] = values
+    o++
+    arr2 = {values: values, class: playr}
+    gameStats.push(arr2)
+    console.log(gameStats)
+    o++
+
+    console.log("count in sets:", count)
+}
+
+function bugFix() {
+    const moreThan = (arr, num) => {
+        const creds = arr.reduce((acc, val) => {
+           let { map, res } = acc;
+           const count = map.get(val);
+           if(!count && typeof count !== 'number'){
+              map.set(val, 1);
+           }else if(num - count <= 1){
+              res.push(val);
+           } else {
+              map.set(val, count+1);
+           };
+           return {map, res};
+        }, {
+           map: new Map(),
+           res: []
+        });
+        return creds.res;
+     };
+    let arr3 = moreThan(arr,1)
+    console.log(arr3)
+
+    if (arr3.length > 0){
+
+        if (arr3.length > 0){
+        document.getElementById('twice').innerHTML = "DO NOT CLICK TWICE MONKEY."
+        setTimeout(useless,2000)
+        }
+        if(playerTurn == player1){
+            document.getElementById(arr3[0]).innerHTML = player1
+            count--
+            //window.setTimeout(loser,3000)
+            goToPrevious()
+        }
+        else if(playerTurn == player2){
+            document.getElementById(arr3[0]).innerHTML = player2
+            count--
+            //window.setTimeout(loser,3000)
+            goToPrevious()
+        }
+    }
+}
+
+function useless() {
+    document.getElementById('twice').innerHTML = null
+}
 
 let one = document.getElementById("1").innerText;
 let two = document.getElementById("2").innerText;
@@ -120,11 +193,25 @@ function checksBefore(){
     nine = document.getElementById("9").innerText
 }
 
+function goToPrevious(){
+    
+    console.log(gameStats)
+    gameStats.pop()
+    console.log(gameStats)
+    for (var i = 0;i < gameStats.length; i++){ 
+        document.getElementById(gameStats[i].values).className = gameStats[i].class
+        document.getElementById(gameStats[i].values).innerText = gameStats[i].class
+        console.log("count in gotoprev",count)
+
+    }
+    removes(arr.splice(-1))
+    
+}
 
 let playerWins = null
 
 function checks(player){
-    console.log("type of one",typeof one, one)
+    //console.log("type of one",typeof one, one)
     // 1,2,3
     if ((one === "x")&&(two === "x")&& (three === "x")){
         console.log("we have a winner", player)
@@ -228,7 +315,10 @@ function checks(player){
     }
     
 }
-
+function loser(){
+    document.getElementById("twice").innerText = `Due to your incompetence, the game has to be restarted`
+    window.setTimeout(restart, 5000) 
+}
 
 function winner(player){
     //document.getElementById("game-container").innerHTML = null
